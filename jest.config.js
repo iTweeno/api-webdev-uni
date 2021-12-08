10dcfde774df86d4577162251a0f1e3a8cf5c2c0
Tweeno <itweeno@gmail.com>
2021-12-08 16:52:24 +0000

    tests

--- jest.config.js
+++
@@ -1,4 +0,0 @@
-export const transformIgnorePatterns = ['node_modules/(?!(sucrase)/)'];
-export const transform = {
-    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
-};
---
+++ tests/db.test.js
@@ -0,0 +1,41 @@
+const mocked_db = {
+  user: [{ id: 1, first_name: "Joao", last_name: "Silva" }],
+};
+
+it("Find User", async () => {
+  let user_details = {
+    id: 1,
+  };
+
+  let user = mocked_db.user.find((e) => e.id === 1);
+
+  expect(user.first_name).toBe("Joao");
+});
+
+it("Insert user", async () => {
+  let user_details = {
+    id: 1,
+  };
+
+  let user = { id: 2, first_name: "Andre", last_name: "Ferreira" };
+
+  mocked_db.user.push(user);
+
+  expect(user.first_name).toBe("Andre");
+});
+
+it("Remove user", async () => {
+  let user_details = {
+    id: 1,
+  };
+
+  mocked_db.user.splice(mocked_db.user.indexOf(mocked_db.user.find((e) => e.id === user_details.id)), 1);
+
+  expect(mocked_db.user).not.toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: 1,
+      })
+    ])
+  );
+});
